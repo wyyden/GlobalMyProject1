2598ffe18cee9d33b8c76a679599b9d2927e6af2
wyyden <ankudinoff.daniil@yandex.ru>
2021-11-08 19:56:21 +0300

    Create files

---
+++ funcs.cpp
@@ -0,0 +1,15 @@
+//funcs.cpp
+#include "stdafx.h"
+#include "funcs.h"
+double MySum(double a, double b)
+{
+return a + b;
+}
+double MySub(double a, double b)
+{
+return a - b;
+}
+double MyMul(double a, double b)
+{
+return a * b;
+}
